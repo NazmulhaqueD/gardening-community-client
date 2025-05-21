c7f85b7bed22637fd33e516880e10e0f13bff78c
NazmulhaqueD <mdnajmulhaque982@gmail.com>
2025-05-21 14:28:31 +0600

    sign up emailPassword and google signUp completed now

--- src/Auth/AuthProvider.jsx
+++ src/Auth/AuthProvider.jsx
@@ -1,23 +1,29 @@
 import React, { createContext, useState } from 'react';
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
 import { app } from './firebase.config';
 
 export const AuthContext = createContext();
+const provider = new GoogleAuthProvider();
 const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
 
-    const [user, setUser]=useState('');
+    const [user, setUser] = useState('');
 
-    const signUp = (email, password)=>{
+    const signUp = (email, password) => {
         return createUserWithEmailAndPassword(auth, email, password);
     }
-    const signIn = (email, password)=>{
+    const signInWithGoogle = () => {
+        return signInWithPopup(auth, provider);
+    }
+    const signIn = (email, password) => {
         return signInWithEmailAndPassword(auth, email, password);
     }
 
+
     const userInfo = {
         signUp,
+        signInWithGoogle,
         signIn,
         user,
         setUser,
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { NavLink } from 'react-router';
 
 const Login = () => {
+
+
+
     return (
-        <div>
-            login
+
+        <div className="card bg-base-100 max-w-sm mx-auto my-24 shrink-0 shadow-2xl">
+            <h1 className="text-5xl font-bold text-center">Login now!</h1>
+            <div className="card-body">
+                <form className='space-y-4'>
+                    <label className="label">Email</label>
+                    <input type="email" className="input" name='email' placeholder="Email" />
+                    <label className="label">Password</label>
+                    <input type="password" className="input" name='password' placeholder="Password" />
+                    <div><a className="link link-hover">Forgot password?</a></div>
+                    <button className="btn w-full btn-neutral mt-4">Login</button>
+                    <p>Don't have an account !!! please
+                        <NavLink to='/register' className={'text-teal-500 px-2 text-xl font-semibold'}>Register</NavLink>
+                    </p>
+                </form>
+            </div>
         </div>
     );
 };
