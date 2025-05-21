fba04b8a2e6d9181dadd1fa807f4f14c48f7768a
NazmulhaqueD <mdnajmulhaque982@gmail.com>
2025-05-21 12:45:22 +0600

    environment variable setup now

--- src/Auth/AuthProvider.jsx
+++ src/Auth/AuthProvider.jsx
@@ -1,12 +1,27 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { app } from './firebase.config';
 
 export const AuthContext = createContext();
+const auth = getAuth(app);
 
-const AuthProvider = ({children}) => {
+const AuthProvider = ({ children }) => {
 
-const userInfo = {
-    name: 'nazmul'
-}
+    const [user, setUser]=useState('');
+
+    const signUp = (email, password)=>{
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+    const signIn = (email, password)=>{
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const userInfo = {
+        signUp,
+        signIn,
+        user,
+        setUser,
+    }
 
     return (
         <AuthContext value={userInfo}>
