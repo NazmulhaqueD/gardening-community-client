f8c024a55be161afe83d036f50b9baaac6239a71
NazmulhaqueD <mdnajmulhaque982@gmail.com>
2025-05-22 14:56:47 +0600

    myTips delete confirmation and deleted in db and client site imidiat done

--- src/Pages/MyTips.jsx
+++ src/Pages/MyTips.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../Auth/AuthProvider';
 import { NavLink } from 'react-router';
+import Swal from 'sweetalert2';
 
 
 const MyTips = () => {
@@ -16,6 +17,40 @@ const MyTips = () => {
                 setMyTips(data)
             })
     }, [user])
+
+    const handleTipsDelete = (id) => {
+
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/myTips/${id}`, { method: 'DELETE' })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.deletedCount) {
+                            const remaining = myTips.filter(single => single._id !== id);
+                            console.log(data)
+                            setMyTips(remaining);
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    })
+            }
+        });
+
+    }
+
+
+
     return (
         <div className="overflow-x-auto bg-base-300 py-8 bg">
             <h1 className='text-success text-3xl md:text-4xl my-8 text-center font-bold'> My Garden Tips (total: {myTips?.length})</h1>
@@ -32,16 +67,16 @@ const MyTips = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    { 
-                        myTips?.map(tips=><tr
-                        key={tips._id}
+                    {
+                        myTips?.map(tips => <tr
+                            key={tips._id}
                         >
                             <td className='py-10'>{tips.title}</td>
                             <td>{tips.category}</td>
                             <td>{tips.availability}</td>
                             <td>12/08/2025</td>
                             <td><NavLink className={'btn btn-success'}>Update</NavLink></td>
-                            <td><button className='btn btn-error'>Delete</button></td>
+                            <td><button onClick={() => handleTipsDelete(tips._id)} className='btn btn-error'>Delete</button></td>
                         </tr>)
                     }
                 </tbody>
